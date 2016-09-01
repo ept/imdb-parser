7450eae0967d9cfd402b217953bcb4734b1e1936
Martin Kleppmann <martin@kleppmann.com>
2016-09-01 14:47:55 +0100

    Tidy up build of jar files with dependencies

--- src/main/java/uk/ac/cam/cl/databases/moviedb/internal/RelationalUI.java
+++ src/main/java/uk/ac/cam/cl/databases/moviedb/internal/RelationalUI.java
@@ -6,12 +6,15 @@ import org.hsqldb.util.DatabaseManagerSwing;
 public class RelationalUI {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.err.println("Usage: java -jar relational-db.jar <path-to-extract-database>");
+            System.err.println("Usage: java -jar relational-db.jar <path-to-data-directory>");
             System.exit(1);
         }
 
         File dataDir = new File(args[0]);
-        if (!dataDir.exists()) dataDir.mkdirs();
+        if (!dataDir.exists()) {
+            System.err.println("Data directory does not exist: " + dataDir.getAbsolutePath());
+            System.exit(1);
+        }
 
         DatabaseManagerSwing.main(new String[] {
             "--url", "jdbc:hsqldb:file:" + new File(dataDir, "moviedb") + ";shutdown=true"
